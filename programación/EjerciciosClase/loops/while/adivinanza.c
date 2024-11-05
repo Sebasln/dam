86f6f5ac734a6d575a83d6d691c72c76c342448e
Sebas Silva Narro <sebassilvanarro@gmail.com>
2024-11-05 14:30:49 +0100

    añadiendo lo poquillo de structs

--- programación/EjerciciosClase/loops/while/adivinanza.c
+++ programación/EjerciciosClase/loops/while/adivinanza.c
@@ -3,14 +3,16 @@
 #include <time.h>
 
 int main() {
-	srand(time(NULL));
-	int randomNumber = rand() % 100;
+	srand(time(NULL)); //cambia cada segundo
+	int randomNumber = (rand() % 100) + 1; //se le suma 1 porque va de 0 a 99. //se le suma 1 porque va de 0 a 99.
+	//modulo 100 da numeros entre 0 y 99
 	int num = 150;
 	
 	printf("Adivina el número en el que estoy pensando: ");
 	scanf("%d", &num);
 
-	printf("%d", randomNumber);
+	printf("%d\n", randomNumber);
+
 	while(randomNumber != num){
 		printf("Vuelvelo a intentar: ");
 		scanf("%d", &num);
---
+++ programación/EjerciciosClase/struct/estudiantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct estudiantes{
+	char nombre[50];
+	int edad;
+	float promedio;
+};
+
+int main(){
+	struct estudiantes
+
+}
